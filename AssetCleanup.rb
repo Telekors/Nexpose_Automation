dc49f0e48ec4bf0a185f6551919de120d2bb6be9
Telekors <38220388+Telekors@users.noreply.github.com>
2018-04-09 11:18:57 -0500

    Create AssetCleanup.rb

---
+++ AssetCleanup.rb
@@ -0,0 +1,36 @@
+require 'nexpose'
+require 'yaml'
+include Nexpose
+
+## Put time connected to console (This function can be disabled its just there for comfort)
+puts "Production Console: #{Time.now.strftime("%d/%m/%Y %H:%M")}"
+
+## Set $settings variable to config file
+$settings = YAML::load_file 'settings.yml'
+
+## Reference the config file 'settings.yml'
+NexposeIP = $settings[:nexpose][:host]
+NexposeUN = $settings[:nexpose][:user]
+NexposePW = $settings[:nexpose][:pass]
+
+## New Nexpose Connection -> Requirements are Device IP, Username, Password
+nsc = Nexpose::Connection.new(NexposeIP, NexposeUN, NexposePW)
+nsc.login
+
+#puts nsc.system_information
+ag = AssetGroup.load(nsc, 90)  ##Change 90 to match your asset group id (this can be found by browsing here https://YOURNEXPOSE:3780/asset/group/listing.jsp and hovering over the group you want to reference )
+
+
+
+##Loads all the referenced devices into a variable and iterates through deleting them
+devices = ag.devices
+
+devices.each do |host|
+  dev_id = host.id
+# output << ("#{host.address}\n")
+ nsc.delete_device(dev_id)
+end
+#output.close
+
+## Log out of your nexpose console. 
+nsc.logout
